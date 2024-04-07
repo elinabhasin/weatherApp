57243f8d97c8bf058ee15f964a3c6232f21fdbc6
elinabhasin <elinabhasin27@gmail.com>
2024-04-08 00:37:07 +0530

    Added dynamic data for different cities

--- weather.js
+++ weather.js
@@ -26,22 +26,107 @@ document.addEventListener("DOMContentLoaded", () => {
             // document.getElementById('quote').innerText=response.quote;
             // document.getElementById('author').innerText=response.author;
         });
-    
+        
+		let cityInput;
 		function updateCity(){
-			const cityInput = document.getElementById("city").value;
+			cityInput = document.getElementById("city").value;
 			document.getElementById("cityName").innerText = cityInput;
 		};
 
     const searchButton = document.getElementById("search");
-    searchButton.addEventListener("click", (e) => {
-        e.preventDefault();
-		updateCity();
-    });
 	
-	city.addEventListener("keypress", (e) => {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            updateCity();
-        }
-    });
+const choices = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cedc0ee057msh8166079f341b2e1p1c60b3jsn82bd7e78b951',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city) => {
+    fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, choices)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            temp.innerHTML = response.temp;
+            feelsLike.innerHTML = response.feels_like;
+            humidval.innerHTML = response.humidity;
+            windval.innerHTML = response.wind_speed;
+        })
+        .catch(err => console.error(err));
+};
+const Options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cedc0ee057msh8166079f341b2e1p1c60b3jsn82bd7e78b951',
+		'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const updateAQI=(city)=>{
+	fetch(`https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=${city}`, Options)
+	.then(response => response.json())
+	.then((response) => {
+		console.log(response);
+		const overAll=response.overall_aqi;
+		if(0<=overAll && overAll<=50){
+           aqival.innerHTML="GOOD";
+		   aqival.style.color="green";
+		}
+		else if(51<=overAll && overAll<=100){
+			aqival.innerHTML="MODERATE";
+			aqival.style.color="#FEBE10";
+		}
+		else if(101<=overAll && overAll<=150){
+			aqival.innerHTML="UNHEALTHY FOR SENSITIVE GROUPS";
+			aqival.style.color="orange";
+		}
+		else if(151<=overAll && overAll<=200){
+			aqival.innerHTML="UNHEALTHY";
+			aqival.style.color="red";
+		}
+		else if(201<=overAll && overAll<=300){
+			aqival.innerHTML="VERY UNHEALTHY";
+			aqival.style.color="purple";
+		}
+		else{
+			aqival.innerHTML="HAZARDOUS";
+			aqival.style.color="maroon";
+		}
+		
+	})
+	.catch(err => console.error(err));
+};
+searchButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	updateCity();
+	getWeather(cityInput);
+	updateAQI(cityInput);
 });
+city.addEventListener("keypress", (e) => {
+	if (e.key === "Enter") {
+		e.preventDefault();
+		updateCity();
+		getWeather(cityInput);
+		updateAQI(cityInput);
+	}
+});
+getWeather("Delhi");
+updateAQI("Delhi");
+
+
+const apiKey = '365f8047c54239bd3456ddb23bb7fc16'; // Replace with your OpenWeatherMap API key
+
+const apiUrl = `https://api.openweathermap.org/data/2.5/air_pollution?q=${'Delhi'}&appid=${apiKey}`;
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Do something with the air pollution data
+    })
+    .catch(error => {
+        console.log('Error fetching air pollution data:', error);
+    });
+
+
+
+});
+
