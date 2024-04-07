e0cd954e4c0400c63545344c931f6b72b75cd3d6
elinabhasin <elinabhasin27@gmail.com>
2024-04-07 17:37:40 +0530

    City name changes with keypress and search

--- weather.js
+++ weather.js
@@ -1,34 +1,47 @@
-let currentTime = new Date();
-let hours = currentTime.getHours();
-console.log(hours);
+document.addEventListener("DOMContentLoaded", () => {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    console.log(hours);
 
-if(0<=hours && hours<12){
-    document.getElementById("greet").innerText="Good Morning!";
-}else if(12<=hours && hours<16){
-    document.getElementById("greet").innerText="Good Afternoon!";
-}else{
-    document.getElementById("greet").innerText="Good Evening!";
-}
+    if (0 <= hours && hours < 12) {
+        document.getElementById("greet").innerText = "Good Morning!";
+    } else if (12 <= hours && hours < 16) {
+        document.getElementById("greet").innerText = "Good Afternoon!";
+    } else {
+        document.getElementById("greet").innerText = "Good Evening!";
+    }
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cedc0ee057msh8166079f341b2e1p1c60b3jsn82bd7e78b951',
-		'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
-	}
-};
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cedc0ee057msh8166079f341b2e1p1c60b3jsn82bd7e78b951',
+            'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
+        }
+    };
 
-fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=environmental',options)
-.then(response=>response.json())
-.then((response)=>{
-    console.log(response);
-	// document.getElementById('quote').innerText=response.quote;
-	// document.getElementById('author').innerText=response.author;
-});
+    fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=environmental', options)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            // document.getElementById('quote').innerText=response.quote;
+            // document.getElementById('author').innerText=response.author;
+        });
+    
+		function updateCity(){
+			const cityInput = document.getElementById("city").value;
+			document.getElementById("cityName").innerText = cityInput;
+		};
 
-const cityInput=document.getElementById("citySearch");
-
-form.addEventListener("click", (e) => {
-    e.preventDefault(); 
-	document.getElementById("cityName").innerText=cityInput;
+    const searchButton = document.getElementById("search");
+    searchButton.addEventListener("click", (e) => {
+        e.preventDefault();
+		updateCity();
+    });
+	
+	city.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            updateCity();
+        }
+    });
 });
